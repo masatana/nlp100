24f7ee182dce06e27fefa23d96707c646ec3e4b7
masatana <plaza.tumbling@gmail.com>
2014-05-18 19:10:38 -0700

    Delete unused script

--- 1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# $ wc -l address.txt
-
-with open("./address.txt") as f:
-    print(len(f.read().strip().split("\n")))
--- 2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# $ TODO
-
-with open("./address.txt") as f:
-    for line in f:
-        print(line.strip().replace("\t", " "))
